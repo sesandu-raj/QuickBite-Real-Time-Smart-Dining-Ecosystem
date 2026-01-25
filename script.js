1c11356835f250689b4873f68e894838ce19825f
Sesandu <sesanduj@gmail.com>
2026-01-25 12:29:55 +0530

    CST24051: Added Scroll animation

--- script.js
+++ script.js
@@ -128,5 +128,29 @@ function saveName(table) {
   }
 }
 
+// Scroll Animation Handler
+function initScrollAnimations() {
+  const observerOptions = {
+    threshold: 0.1,
+    rootMargin: "0px 0px -50px 0px",
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  }, observerOptions);
+
+  // Observe all animate-on-scroll elements
+  document.querySelectorAll(".animate-on-scroll").forEach((el) => {
+    observer.observe(el);
+  });
+}
+
 // Run on page load
-window.onload = initializeQuickBite;
+window.onload = function () {
+  initializeQuickBite();
+  initScrollAnimations();
+};
